da8972aa884daf18e2e7000f3f77eb93e464a749
senthilkumarq <43035170+senthilkumarq@users.noreply.github.com>
2025-07-02 08:44:45 +0530

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,15 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+
+  reporter: [
+  ['html', { open: 'always' }], // Opens HTML report automatically
+  ['allure-playwright', { outputFolder: 'custom-allure-results' }], //  Allure advanced reporting
+  ['list'], // Minimal terminal output
+  ['json', { outputFile: 'report.json' }], //  JSON file with test details
+  ['junit', { outputFile: 'results.xml' }] //  JUnit XML for CI
+],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
